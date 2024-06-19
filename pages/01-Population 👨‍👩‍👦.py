2793c08dedc92987c97bce1a1f88ed050f68c111
huynhtranyennhintake2023 <10623034@student.vgu.edu.vn>
2024-06-19 13:55:13 +0700

    x

--- pages/01-Population 👨‍👩‍👦.py
+++ pages/01-Population 👨‍👩‍👦.py
@@ -111,11 +111,48 @@ df1b = df1a.merge(df1, how = 'inner')
 fig1 = px.bar(df1b, x = 'country', y = 'pop', color = 'country', 
                 labels = {'pop':'Population', 'country':'Country', 'pop_y':'Population'}, width = 950, height = 600, text = 'pop')
 
-fig1.update_traces(texttemplate='%{text:.2s}', textposition='outside')
-fig1.update_layout(uniformtext_minsize=8, xaxis_tickangle=45, showlegend = False)
-fig1.update_layout(font_family="Courier New",font = dict (size = 20))
+fig1.update_traces(texttemplate='%{text:.3s}', textposition='outside')
+fig1.update_layout(uniformtext_minsize=6, xaxis_tickangle=45, showlegend = False)
+fig1.update_layout(font_family="Courier New",font = dict (size = 18))
 #fig1.update_traces(textfont_size=12, textangle=45,marker_line_width = 1, marker_line_color = 'white')
 st.plotly_chart(fig1, theme = "streamlit", use_container_width=True)'''
+
+st.markdown("---")
+
+df1b['continent_population'] = df1b['pop'].sum()
+df1b['pop_percentage'] = (df1b['pop']/df1b['continent_population'])*100
+df1b['per_sum'] = df1b['pop_percentage'].sum()
+df1b['per_other'] = df1b['per_sum'] - df1b['pop_percentage']
+
+countries = df1b['country'].unique()
+selected_country = st.selectbox('Choose a country:', countries)
+sub1c =df1b[(df1b['country'] == selected_country)]
+df1c = sub1c.loc[:,['pop', 'country', 'year', 'continent']]
+
+df1d = df1c.merge(df1b, how = 'outer')
+df1e = df1c.merge(df1b, how = 'inner')
+   
+
+def country_class(value):
+       if value == selected_country:
+            return selected_country
+       if value != selected_country:
+            return 'Others'
+
+df1d['country2'] = df1d['country'].map(country_class)
+
+    #df1d.loc[df1d['country'] != selected_country, 'country'] = 'Others'
+    #df1d = pd.DataFrame(pd.pivot_table(df1d, values = 'pop', index='country', aggfunc = 'sum'))
+df1d = df1d.groupby("country2").sum()
+df1d['country2'] = df1d['country'].map(country_class)    
+    
+    #df['percentage']= df.apply(lambda row: ())
+fig1b = px.pie(df1d, values = "pop", names = "country2", width = 800, hole = .3, opacity = 0.8, template='gridon')
+    #fig1b = go.Figure(data = [go.Pie(labels = df1d[0:], values = df1d[1:])])
+fig1b.update_layout(uniformtext_minsize=8, xaxis_tickangle=45, showlegend = True)
+fig1b.update_traces(textfont_size=20,marker=dict( line=dict(color='#000000', width=2)))
+fig1b.update_traces(pull=0.05)
+st.plotly_chart(fig1b, theme = "streamlit", use_container_width=True)
 
 st.code(code, language='python')
 
