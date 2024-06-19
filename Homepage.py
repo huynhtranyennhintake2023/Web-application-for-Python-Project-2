4be77b6e0ab210039cb50eac9a6d6f0e23a43a3b
huynhtranyennhintake2023 <10623034@student.vgu.edu.vn>
2024-06-19 12:23:23 +0700

    x

--- Homepage.py
+++ Homepage.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 import datetime
-import time
+
 
 st.set_page_config(page_title="Web application", layout="wide", page_icon="📍")
 st.balloons()
