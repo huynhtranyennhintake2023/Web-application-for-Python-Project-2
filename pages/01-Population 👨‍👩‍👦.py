8522d3d64f4c6158890658133cbc4ca012e1f189
huynhtranyennhintake2023 <10623034@student.vgu.edu.vn>
2024-06-19 16:12:55 +0700

    a

--- pages/01-Population 👨‍👩‍👦.py
+++ pages/01-Population 👨‍👩‍👦.py
@@ -94,7 +94,8 @@ fig1b.update_traces(textfont_size=20,marker=dict( line=dict(color='#000000', wid
 fig1b.update_traces(pull=0.05)
 st.plotly_chart(fig1b, theme = "streamlit", use_container_width=True)
  
-expander = st.expander("***Are you curious about the code to draw this plot?***") 
+st.header("***Are you curious about the code to draw these plots?***")
+expander = st.expander("***Click this box***")  
 expander.write ('''
 import streamlit as st
 import plotly.express as px
