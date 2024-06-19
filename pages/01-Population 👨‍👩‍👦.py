45a3083f1f47741c89300fcdf2a28aad5a5c2657
huynhtranyennhintake2023 <10623034@student.vgu.edu.vn>
2024-06-19 14:04:52 +0700

    x

--- pages/01-Population 👨‍👩‍👦.py
+++ pages/01-Population 👨‍👩‍👦.py
@@ -56,32 +56,6 @@ fig1.update_layout(font_family="Courier New",font = dict (size = 50))
 #fig1.update_traces(textfont_size=12, textangle=45,marker_line_width = 1, marker_line_color = 'white')
 st.plotly_chart(fig1, theme = "streamlit", use_container_width=True)
 
-st.markdown("---")
-
-option1 = st.selectbox('**Please choose a continent:**',
-('Asia', 'Africa', 'Europe','Americas','Oceania'))
-st.caption(f"You selected: {option1}")
-sub1= df[(df["continent"]==option1)]
-df1 = sub1.loc[:,['year','pop', 'country', 'iso_alpha']]
-
-years = df['year'].unique()
-selected_year = st.selectbox('**Choose a country:**', years)
-sub1a =df[(df['year'] == selected_year)]
-df1a = sub1a.loc[:,['pop', 'country', 'year']]
-
-#df1b = df1a.merge(df1, how = 'inner', on = 'country')
-df1b = df1a.merge(df1, how = 'inner')
-#st.dataframe(df1b)
-#fig1 = px.scatter_geo(df3, locations="iso_alpha", color="country",hover_name="country",
-#                      size="pop",animation_frame="year",projection="natural earth", title = 'Population', width = 700, height = 800)
-fig1 = px.bar(df1b, x = 'country', y = 'pop', color = 'country', 
-                labels = {'pop':'Population', 'country':'Country', 'pop_y':'Population'}, width = 950, height = 600, text = 'pop')
-
-fig1.update_traces(texttemplate='%{text:.3s}', textposition='outside')
-fig1.update_layout(uniformtext_minsize=6, xaxis_tickangle=45, showlegend = False)
-fig1.update_layout(font_family="Courier New",font = dict (size = 18))
-#fig1.update_traces(textfont_size=12, textangle=45,marker_line_width = 1, marker_line_color = 'white')
-st.plotly_chart(fig1, theme = "streamlit", use_container_width=True)
 
 st.markdown("---")
 
