1027b80893f855f4b32207436f5c178295788476
huynhtranyennhintake2023 <10623034@student.vgu.edu.vn>
2024-06-24 20:14:17 +0700

    x

--- Homepage.py
+++ Homepage.py
@@ -51,7 +51,7 @@ with tab1:
         st.write("My name is Huỳnh Trần Yến Nhi. I'm a freshman from :orange[**Vietnamese-German University**]. My major is Business Administration or Betriebswirtschaftslehre in German, intake 2023.")
 
     with col2:
-        st.lottie("http://www.fanpop.com/clubs/penguins-of-madagascar/images/37800672/title/hello-photo")
+        st.lottie("http://www.fanpop.com/clubs/penguins-of-madagascar/images/37800672/title/hello-photo", key = "hello")
 
 
 with tab2: 
