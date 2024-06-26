6ab74102aa953249fd4ed0c697eddf286ba93676
huynhtranyennhintake2023 <10623034@student.vgu.edu.vn>
2024-06-26 20:40:22 +0700

    x

--- Homepage.py
+++ Homepage.py
@@ -27,6 +27,28 @@ background-color: rgba(0,0,0,0);
 </style>
 """
 st.markdown(bgr_img,unsafe_allow_html= True)
+
+bgr_img ="""
+<style>
+[data-testid="stSidebar"]
+{
+background-image: url("https://th.bing.com/th/id/OIP.JMY19P95m7wMuf0_qKLGtwHaEo?w=305&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7");
+background-size:cover;
+}
+
+[data-testid="stHeader"]
+{
+background-color: rgba(0,0,0,0);
+}
+
+[data-testid="stSidebar"]
+{
+background-color: rgba(0,0,0,0);
+}
+</style>
+"""
+st.markdown(bgr_img,unsafe_allow_html= True)
+
 st.divider()
 
 tab1, tab2 = st.tabs(["Introduction", "Gapminder dataset"])
